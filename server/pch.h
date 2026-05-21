06e26fa097331a2bf3c4d33e8ff1e101e09d9232
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2026-05-21 19:13:25 +0900

    SESSION에서 사용할 OVERLAPPED 구조체 선언

--- server/pch.h
+++ server/pch.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "protocol_2026.h"
-#include <winsock2.h>
-#include <windows.h>
-#include <ws2tcpip.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
 #include <iostream>
 #include <unordered_map>
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <chrono>
+#include <queue>
 #include <tbb/concurrent_unordered_map.h>
+
+#pragma comment(lib, "MSWSock.lib")
+#pragma comment(lib, "WS2_32.lib")

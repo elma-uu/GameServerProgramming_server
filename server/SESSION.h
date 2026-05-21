5c44b36db851f00a89f5e70ca73265bc3b2ff2bf
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2026-05-21 19:21:56 +0900

    main에 clients 컨테이너 생성
    intel tbb::concurrent_unordered_map

--- server/SESSION.h
+++ server/SESSION.h
@@ -6,19 +6,18 @@ enum CL_STATE { CS_CONNECT, CS_PLAYING, CS_LOGOUT };
 
 class SESSION {
 public:
-	SOCKET m_client;
-	int m_id;
-	CL_STATE m_state;
-	EXP_OVER m_recv_over;
-	int m_prev_recv;
-	char m_username[MAX_NAME_LEN];
-	short m_x, m_y;
-	int m_move_time;
-	int m_sector_id;
+	SOCKET mClient;
+	int mId;
+	CL_STATE mState;
+	EXP_OVER mRecv_over;
+	int mPrev_recv;
+	char mUsername[MAX_NAME_LEN];
+	short mX, mY;
+	int mMove_time;
+	int mSector_id;
 	std::unordered_set<int> m_visible_players;
 	std::unordered_set<int> m_visible_npcs;
 	std::mutex m_visible_mutex;
 	bool is_player;
-
 };
 
--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
+#include "SESSION.h"
 
+tbb::concurrent_unordered_map<int, std::shared_ptr<SESSION>> clients;
 
 int main()
 {

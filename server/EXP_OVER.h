ba01aa403087b28b1663cee3a9c0c4359019da5e
elma_uu <ansvia@naver.com>
2026-05-22 14:12:30 +0900

    순환참조 해결 및 RingBuffer 구현

--- server/EXP_OVER.h
+++ server/EXP_OVER.h
@@ -1,7 +1,36 @@
 #pragma once
-#include "pch.h"
+#include "common.h"
 
 enum IOType { IO_SEND, IO_RECV, IO_ACCEPT };
+
+class RingBuffer {
+	public:
+	char buffer[MAX_BUF_SIZE];
+	int head;
+	int tail;
+	int size;
+	RingBuffer() : head(0), tail(0), size(0) {}
+	bool push(const char* data, int len) {
+		if (len > MAX_BUF_SIZE - size) return false; // Not enough space
+		for (int i = 0; i < len; ++i) {
+			buffer[tail] = data[i];
+			tail = (tail + 1) % MAX_BUF_SIZE;
+		}
+		size += len;
+		return true;
+	}
+	int pop(char* dest, int len) {
+		if (len > size) return -1; // Not enough data
+		for (int i = 0; i < len; ++i) {
+			dest[i] = buffer[head];
+			head = (head + 1) % MAX_BUF_SIZE;
+		}
+		size -= len;
+		return len;
+	}
+	int get_size() const { return size; }
+};
+
 
 class EXP_OVER {
 public:
@@ -9,17 +38,17 @@ public:
 	IOType  m_iotype;
 	WSABUF	m_wsa;
 	SOCKET  m_client_socket;
-	char  m_buff[MAX_BUF_SIZE];
+	RingBuffer m_ring_buffer;
 	EXP_OVER()
 	{
 		ZeroMemory(&m_over, sizeof(m_over));
-		m_wsa.buf = m_buff;
+		m_wsa.buf = m_ring_buffer.buffer;
 		m_wsa.len = MAX_BUF_SIZE;
 	}
 	EXP_OVER(IOType iot) : m_iotype(iot)
 	{
 		ZeroMemory(&m_over, sizeof(m_over));
-		m_wsa.buf = m_buff;
+		m_wsa.buf = m_ring_buffer.buffer;
 		m_wsa.len = MAX_BUF_SIZE;
 	}
 };
--- server/pch.h
+++ server/pch.h
@@ -1,14 +1,13 @@
 #pragma once
 #include "common.h"
-#include "SESSION.h"
+
+class SESSION;
 
 constexpr int VIEW_RANGE = 15;
 constexpr int SECTOR_SIZE = 10;
 
 tbb::concurrent_unordered_map<int, std::shared_ptr<SESSION>> clients;
 tbb::concurrent_unordered_map<int, std::unordered_set<int>> sectors;
-
-enum CL_STATE { CS_CONNECT, CS_PLAYING, CS_LOGOUT };
 
 
 SOCKET g_server;
@@ -29,9 +28,39 @@ void error_display(const wchar_t* msg, int err_no)
 	LocalFree(lpMsgBuf);
 }
 
+//---------
+// Sector
+//---------
 inline int get_sector_id(short x, short y)
 {
 	int sector_x = x / SECTOR_SIZE;
 	int sector_y = y / SECTOR_SIZE;
 	return sector_y * ((WORLD_WIDTH / SECTOR_SIZE) + 1) + sector_x;
+}
+
+void disconnect(int key);
+
+#include "SESSION.h"
+
+inline void disconnect(int key)
+{
+	std::cout << "client " << key << " disconnected" << std::endl;
+	std::shared_ptr<SESSION> cla = clients[key];
+	if (nullptr != cla)
+	{
+		cla->mState = CS_LOGOUT;
+		sectors[cla->mSector_id].erase(key);
+		auto visible_copy = cla->m_visible_players;
+		for (auto& other : visible_copy)
+		{
+			std::shared_ptr<SESSION> o = clients[other];
+			if (nullptr == o) continue;
+			if (o->mState == CS_PLAYING)
+				o->sendRemovePlayer(key);
+		}
+		if (cla->mClient != INVALID_SOCKET)
+			closesocket(cla->mClient);
+		cla->mClient = INVALID_SOCKET;
+	}
+	clients.unsafe_erase(key);
 }

ddf8a46a6d9e0915fadd5d806a7058e9b907c63e
elma_uu <ansvia@naver.com>
2026-05-22 14:12:43 +0900

    SESSION 기본 함수 정의

--- server/SESSION.cpp
+++ server/SESSION.cpp
@@ -1,1 +1,67 @@
 #include "common.h"
+#include "SESSION.h"
+
+SESSION::SESSION()
+{
+	std::cout << "SESSION create error" << std::endl;
+	exit(1);
+}
+
+SESSION::SESSION(SOCKET s, int id)
+{
+	mClient = s;
+	mId = id;
+	mState = CS_CONNECT;
+	mX = 0;
+	mY = 0;
+	mMove_time = 0;
+	mSector_id = 0;
+	is_player = true;
+}
+
+SESSION::SESSION(int id, bool isPlayer)
+{
+	mId = id;
+	mState = CS_CONNECT;
+	mX = 0;
+	mY = 0;
+	mMove_time = 0;
+	mSector_id = 0;
+	is_player = isPlayer;
+}
+
+SESSION::~SESSION()
+{
+}
+
+void SESSION::sendLoginSuccess()
+{
+}
+
+void SESSION::doRecv()
+{
+}
+
+void SESSION::doSend(int numBytes, char* mess)
+{
+}
+
+void SESSION::sendAvatarInfo()
+{
+}
+
+void SESSION::sendAddPlayer(int player_id)
+{
+}
+
+void SESSION::sendRemovePlayer(int player_id)
+{
+}
+
+void SESSION::sendMovePacket(int mover)
+{
+}
+
+void SESSION::processPacket(unsigned char* p)
+{
+}
--- server/SESSION.h
+++ server/SESSION.h
@@ -9,8 +9,7 @@ public:
 	SOCKET mClient;
 	int mId;
 	CL_STATE mState;
-	EXP_OVER mRecv_over;
-	int mPrev_recv;
+	EXP_OVER mOver;
 	char mUsername[MAX_NAME_LEN];
 	short mX, mY;
 	int mMove_time;
@@ -19,5 +18,21 @@ public:
 	std::unordered_set<int> m_visible_npcs;
 	std::mutex m_visible_mutex;
 	bool is_player;
-};
 
+public:
+
+	SESSION();
+	SESSION(SOCKET s, int id);
+	SESSION(int id, bool isPlayer);
+	~SESSION();
+
+
+	void sendLoginSuccess();
+	void doRecv();
+	void doSend(int numBytes, char* mess);
+	void sendAvatarInfo();
+	void sendAddPlayer(int player_id);
+	void sendRemovePlayer(int player_id);
+	void sendMovePacket(int mover);
+	void processPacket(unsigned char* p);
+};
